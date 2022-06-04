9c9766467e1ecfbea609b864362b9651d12c0aef
cabooker <90180280+cabooker@users.noreply.github.com>
2022-06-04 13:22:51 +0300

    again

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,18 @@
 
 /**
 *main - Prints the alphabet in lowercase and then upercase followed by new line
-*Return: always 0 
+*Return: always 0 (Sucess)
 */
 int main(void)
 {
 int ch;
 for (ch = 97; ch <= 122; ch++)
 {
-    putchar(ch);
+putchar(ch);
 }
 for (ch = 65; ch <= 90; ch++)
 {
-    putchar(ch);
+putchar(ch);
 }
 putchar(10);
 
