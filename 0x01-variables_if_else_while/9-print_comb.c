7b122f77173d8305e59a9cd90d1d704c5778cc1f
cabooker <calvbooker@gmail.com>
2022-06-06 01:54:44 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,17 @@
 
 int main(void)
 {
-int ch;
-for (ch = 48; ch <= 57; ch++)
-{
-putchar(ch);
-if (ch != 57)
-putchar(44);
-putchar(32);
-}
+int i = 0;
 
+while (i <= 9)
+{
+putchar(i);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar("\n");
 return (0); }
